b661250998405e1da11c18f6e4a591b49332b647
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-02-08 10:55:51 +0400

    Current results

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,19 +1,17 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
-  // const { data: session } = useSession();
   const searchParams = useSearchParams();
-  const promptId = searchParams.get('id');
+  const promptId = searchParams.get("id");
 
+  const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
-  const [post, setPost] = useState({ prompt: "", tag: "" });
 
   useEffect(() => {
     const getPromptDetails = async () => {
@@ -24,16 +22,16 @@ const EditPrompt = () => {
         prompt: data.prompt,
         tag: data.tag,
       });
-    }
+    };
 
     if (promptId) getPromptDetails();
-  }, [promptId])
+  }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    if (!promptId) return alert('Prompt ID not found');
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -65,4 +63,4 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
